a68a3ac93d19fb30a7677b83646e4eb3ae037ea4
gcsamuel <gcsamuelsena@gmail.com>
2026-03-10 12:53:03 -0300

    fix migrate vercel

--- api/index.py
+++ api/index.py
@@ -1,7 +1,15 @@
 import os
+import django
+from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-from django.core.wsgi import get_wsgi_application
+django.setup()
+
+try:
+    call_command("migrate")
+except:
+    pass
 
 app = get_wsgi_application()
